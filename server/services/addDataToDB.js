61d4ed1734d25703f9753666571934b132858627
bnGil <gilbn1994@gmail.com>
2022-06-29 17:09:41 +0300

    finished the script that scrapes the data and inserts it to mongo atlas

--- server/services/addDataToDB.js
+++ server/services/addDataToDB.js
@@ -1,8 +1,18 @@
 import { Attraction } from "../models/Attraction.model.js";
 import { Language } from "../models/Language.model.js";
 
-export const addLanguageDataToDB = (arrOfObj, languageCode) => {
-  const attractions = arrOfObj.map((attraction) => new Attraction(attraction));
-  const newLanguage = new Language({ languageCode, attractions });
-  console.log(newLanguage);
+export const addLanguageDataToDB = async (arrOfObj, languageCode) => {
+  try {
+    const attractions = await Promise.all(
+      arrOfObj.map(async (attraction) => {
+        const newAttraction = new Attraction(attraction);
+        await newAttraction.save();
+        return newAttraction;
+      })
+    );
+    const newLanguage = new Language({ languageCode, attractions });
+    await newLanguage.save();
+  } catch (err) {
+    console.log(err.message);
+  }
 };
