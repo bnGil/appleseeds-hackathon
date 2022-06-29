9fe25da045925e7906f3ac9ad96a0cfe4213d534
LiatPardoGrinbaum <pardo.liat@gmail.com>
2022-06-29 15:34:46 +0300

    added quiz component

--- client/src/pages/Quiz/Quiz.jsx
+++ client/src/pages/Quiz/Quiz.jsx
@@ -1,39 +1,69 @@
-//temporary fake data
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Dropdown from "../../components/DropDown/Dropdown";
+import QuizDisplay from "../../components/QuizDisplay/QuizDisplay";
+import "./quiz.css";
+
 const attractionsOfCountry = [
   {
     id: 11,
     country: "China",
     attracionName: "bla bla",
-    imageUrl: "https://www.roadaffair.com/wp-content/uploads/2017/12/angkor-wat-cambodia-shutterstock_167952836.jpg",
+    imageUrl: "https://www.roadaffair.com/wp-content/uploads/2017/12/forbidden-city-china-shutterstock_297209894.jpg",
     description:
       "description description description description description description description description description description description description description description description description description description description description ",
   },
   {
     id: 12,
-    country: "China",
+    country: "spain",
     attracionName: "bla bla 2",
-    imageUrl: "img2.src",
+    imageUrl:
+      "https://www.roadaffair.com/wp-content/uploads/2017/09/park-guell-barcelona-spain-shutterstock_261614468.jpg",
   },
   {
     id: 13,
     country: "China",
     attracionName: "bla bla 3",
-    imageUrl: "img3.src",
+    imageUrl:
+      "https://www.roadaffair.com/wp-content/uploads/2017/12/leshan-giant-buddha-china-shutterstock_197940701.jpg",
   },
   {
     id: 14,
     country: "Germany",
     attracionName: "bla bla 4",
-    imageUrl: "img4.src",
+    imageUrl:
+      "https://www.roadaffair.com/wp-content/uploads/2017/10/neuschwanstein-castle-germany-shutterstock_620565122.jpg",
   },
 ];
 
+// const countries = ["china", "iraq", "indonesia"];
 
 const Quiz = () => {
+  const [dataLng, setDataLng] = useState([]);
+  const [selectedLanguage, setSelectedLanguage] = useState("");
 
+  const [spinner, setSpinner] = useState(false);
+  useEffect(() => {
+    setSpinner(true);
+    // if (selectedLanguage) {
+    //   try {
+    //     const getData = async () => {
+    //       const { data } = await axios.get(`balbla/${selectedLanguage}`);
+    //       setDataLng(data);
+    //       setSpinner(false);
+    //     };
+    //     getData();
+    //   } catch (err) {
+    //     console.log(err);
+    //   }
+    // }
+    setDataLng(attractionsOfCountry);
+  }, []); //i dont want it to change anytime i choose different lang ?
+  console.log(selectedLanguage);
   return (
     <div className="Quiz">
-    QUIZ
+      <Dropdown setSelected={setSelectedLanguage} selectType={"language"} />
+      {selectedLanguage && <QuizDisplay />}
     </div>
   );
 };
