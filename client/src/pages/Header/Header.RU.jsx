e770785e77aae5b6977f4cd0278edae7d0424e55
Mor Menashe <mormen1305@gmail.com>
2022-06-30 12:58:17 +0300

    update

---
+++ client/src/pages/Header/Header.RU.jsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+import LangMenu from "../../components/lang/LangMenu";
+import "./Header.css";
+
+const HeaderRU = () => {
+  return (
+    <div className="Header">
+      <nav>
+        <Link to={`/`}>
+          <div className="logo-container">
+            <div className="img-container">
+              <img src={require("../../assets/favicon.ico")} alt="logo" />
+            </div>
+            <div className="top">
+              <p className="big">BUCKETLIST</p>
+              <p className="small">PLACES TO VISIT IN YOUR LIFETIME</p>
+            </div>
+          </div>
+        </Link>
+        <div className="menu-container">
+          <Link to={`/`}>
+            <div className="home">HOME</div>
+          </Link>
+          <Link to={`/attractions`}>
+            <div className="sites">ATTRACTIONS</div>
+          </Link>
+          <Link to={`/quiz`}>
+            <div className="quiz">QUIZ</div>
+          </Link>
+          <LangMenu />
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default HeaderRU;
